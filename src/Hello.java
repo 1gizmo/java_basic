2e029461ea17f62151bebe9fd9de4832e50acce0
1gizmo <126743255+1gizmo@users.noreply.github.com>
2023-03-27 10:37:09 +0900

    Changes

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        // 실행버튼 ctrl + shift + f10
+        // ctrl + alt + v
+        System.out.println("안녕하세요!");
+
+
+
+
+    }
+}
